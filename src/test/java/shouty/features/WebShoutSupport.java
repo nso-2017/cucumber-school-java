9c4103bdd8904d9bc8a7f96cdeaccf703160fe43
nso-2017 <n.s.oborin@gmail.com>
2017-01-19 15:45:30 +0100

    Updated the file

--- src/test/java/shouty/features/WebShoutSupport.java
+++ src/test/java/shouty/features/WebShoutSupport.java
@@ -3,11 +3,19 @@ package shouty.features;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.By;
+
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import shouty.web.ShoutyServer;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,11 +25,12 @@ public class WebShoutSupport extends ShoutSupport {
 
     private Map<String, WebDriver> browsers = new HashMap<>();
     private WebDriver currentBrowser;
+
     private ShoutyServer server = new ShoutyServer();
 
     @Before
     public void startServer() throws Exception {
-        server.start(getPeople(), 4567);
+        server.start(getPeople(), 3000);
     }
 
     @After
@@ -41,6 +50,7 @@ public class WebShoutSupport extends ShoutSupport {
         loginAs("Sean");
         shout(message);
         rememberMessageShoutedBy(message, "Sean");
+
     }
 
     @Override
@@ -51,7 +61,8 @@ public class WebShoutSupport extends ShoutSupport {
 
     private void loginAs(String personName) {
         currentBrowser = getBrowserFor(personName);
-        currentBrowser.get("http://localhost:4567/?name=" + personName);
+        System.out.println("Getting the stuff /? for " + personName);
+        currentBrowser.get("http://192.168.134.190:3000/?name=" + personName);
     }
 
     private void shout(String message) {
@@ -66,8 +77,54 @@ public class WebShoutSupport extends ShoutSupport {
 
     private WebDriver getBrowserFor(String personName) {
         if (!browsers.containsKey(personName)) {
-            browsers.put(personName, new FirefoxDriver());
+
+
+            //  WebDriver b=new FirefoxDriver();
+            WebDriver b = null;
+            try {
+                b = new FirefoxDriver();
+                if (false) throw new MalformedURLException();
+//                 System.out.println("http://localhost:9515");
+//
+//
+//
+//                Map<String, Object> chromeOptions = new HashMap<String, Object>();
+//                chromeOptions.put("binary", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+//                DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//                capabilities.setCapability("CAPABILITY", chromeOptions);
+//                b= new RemoteWebDriver(new URL("http://localhost:9515"), capabilities);
+//                b.get("http://www.google.com");
+            } catch(MalformedURLException e){
+//                System.out.println("Bad things happen to good people " + e.getStackTrace());
+//
+            }
+
+            browsers.put(personName,b);
+
+
+
         }
         return browsers.get(personName);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
